cf0d84b2abd8b0b2a36b5113eeaf30b77b49c40f
HakobAvagyan <hakobavagyan14@gmail.com>
2025-10-05 02:05:59 +0400

    add home work

---
+++ src/dynamicArray/DynamicArrayDemo.java
@@ -0,0 +1,14 @@
+package dynamicArray;
+
+public class DynamicArrayDemo {
+    public static void main(String[] args) {
+        DynamicArray dynamicArray = new DynamicArray();
+        for (int i = 0; i < 15; i++) {
+            dynamicArray.add(i);
+        }
+        dynamicArray.print();
+        System.out.println(dynamicArray.getByIndex(14));
+        System.out.println(dynamicArray.getByIndex(15));
+
+    }
+}
